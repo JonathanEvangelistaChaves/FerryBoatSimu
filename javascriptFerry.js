234d24a72e1f0c5d4f023edfecfb4e8d51bcbcf8
JonathanEChaves <jonathan_echaves@hotmail.com>
2016-12-05 15:49:00 -0200

    atualiza

--- javascriptFerry.js
+++ javascriptFerry.js
@@ -2,9 +2,9 @@
 
 $(function() {
 
-	var tempoTravessia = 120;
-	var tempoSaidaCarros = 30;
-	var tempoEntradaCarros = 60;
+	var tempoTravessia = 10;//120
+	var tempoSaidaCarros = 20;//30
+	var tempoEntradaCarros = 10;//60
 	
 	var fila = 0;
 	var pos = 1000;
@@ -30,10 +30,16 @@ $(function() {
 	var n = 0;
 	var intervalo;
 	var t = 0;
-	var inicio = 0;
-	var status;
+	var inicioC = 0;
+	var iniSai = 0;
+	var inicioT = 0;
+	var tmp = tempoEntradaCarros;
+	var status = 1;
 	var m;
+	var y = 0;
 	var carrosBalsa = 0;
+	
+	var aux = 480;
 	
 	if($("#balsa1").css("top") == "400px") {
 		$("#balsa1").html(carrosBalsa);
@@ -48,52 +54,33 @@ $(function() {
 		// 3 saida carros
 	
 		intervalo = window.setInterval(function() {
-			console.log(t);
+			//console.log(t);
 			t++;
-			status = 1;
 			if(n == 0) {
 				divPai.append("<div class='carro' id='carro" + n + "' style='left: " + pos + "px;'></div>");
 				vetor[n] = Math.floor((Math.random() * 10) + 2);
-				inicio = t;
+				inicioC = t;
 				n++;
 			} else {
-				//console.log(vetor[n-1]+inicio + " " + t);
-				if((vetor[n-1]+inicio) < t) {
+				//console.log(vetor[n-1]+inicioC + " " + t);
+				if((vetor[n-1]+inicioC) < t) {
 					divPai.append("<div class='carro' id='carro" + n + "' style='left: " + pos + "px;'></div>");
 					vetor[n] = Math.floor((Math.random() * 10) + 2);
-					inicio = t;
+					inicioC = t;
 					m = n;
-					console.log(" " + vetor[n]);
+					//console.log(" " + vetor[n]);
 					n++;
 				}
 			}
 			
-			if(n == 1)
-			{		
-				$("#carro" + (n-1)).animate({
-					left: (480 + (50 * fila)) + "px"
-				}, vetor[n-1]*1000).delay(0).fadeOut(10, function(){
-					$(this).hide();
-				});
-				
-				if($("#balsa1").css("top") == "400px") {
-					$("#balsa1").html(carrosBalsa);
-				} else {
-					$("#balsa2").html(carrosBalsa);
-				}
-				
-				if(fila > 0) {
-					fila--;
-				}
-			} else {
-				if($("#carro" + (n-2)).css("left") == "480px")
-				{
-					$("#carro" + (m)).animate({
+			if(status == 1 && (inicioT + tempoEntradaCarros) > t) {
+				if(n == 1)
+				{		
+					$("#carro" + (n-1)).animate({
 						left: (480 + (50 * fila)) + "px"
-					}, vetor[m]*1000).delay(0).fadeOut(10, function(){
+					}, vetor[n-1]*1000).delay(0).fadeOut(10, function(){
 						$(this).hide();
 					});
-					
 					
 					if($("#balsa1").css("top") == "400px") {
 						$("#balsa1").html(carrosBalsa);
@@ -104,10 +91,106 @@ $(function() {
 					if(fila > 0) {
 						fila--;
 					}
+				} else {
+					if($("#carro" + (n-2)).css("left") == "480px")
+					{
+						$("#carro" + (m)).animate({
+							left: (480 + (50 * fila)) + "px"
+						}, vetor[m]*1000).delay(0).fadeOut(10, function(){
+							$(this).hide();
+						});
+						
+						
+						if($("#balsa1").css("top") == "400px") {
+							$("#balsa1").html(carrosBalsa);
+						} else {
+							$("#balsa2").html(carrosBalsa);
+						}
+					}
 				}
 			}
+			else if(status == 2 && (inicioT + tempoTravessia) > t) {
+				$("#balsa1").animate({
+					left: "440px",
+					top: "400px"
+				}, tempoTravessia*1000);
+				$("#balsa2").animate({
+					left: "440px",
+					top: "80px"
+				}, tempoTravessia*1000);
+			}
+			else if(status == 3 && (inicioT + tempoSaidaCarros) > t) {
+				while(y < tempoSaidaCarros/5){
+					divPai.append("<div class='carro' id='carrosai' style='left: 480px; top: 440px;'></div>");
+					divPai.append("<div class='carro' id='carrosai2' style='left: 480px; top: 100px;'></div>");
+					y++;
+				}
+				$("#carrosai").animate({
+					left: "350px",
+					top: "600px"
+				}, 5000).delay(0).fadeOut(10, function(){
+					$(this).remove();
+				});
+				$("#carrosai2").animate({
+					left: "600px",
+					top: "0px"
+				}, 5000).delay(0).fadeOut(10, function(){
+					$(this).remove();
+				});
+				
+				
+				if(n == 1)
+				{		
+					$("#carro" + (n-1)).animate({
+						left: (480 + (50 * fila)) + "px"
+					}, vetor[n-1]*1000);
+					
+					fila++;
+				} else {
+					if(fila == 0) {
+						if($("#carro" + (n-2)).css("left") == 480 + "px")
+						{
+							$("#carro" + (m)).animate({
+								left: (480 + (50 * fila)) + "px"
+							}, vetor[m]*1000);
+							
+							fila++;
+						}
+					} else {
+						console.log("   " + $("#carro" + (n-2)).css("left"));
+						if($("#carro" + (n-2)).css("left") == aux + "px")
+						{
+							$("#carro" + (m)).animate({
+								left: aux + "px"
+							}, vetor[m]*1000);
+							
+							fila++;
+							aux = aux + 50;
+							console.log("maisa um");
+						}
+					}
+				}
+				
+				
+			}
 			
-			
+			if(t >= tmp+inicioT)
+			{
+				if(status == 1) {
+					status++;
+					tmp = tempoTravessia;
+				}
+				else if(status == 2) {
+					status++;
+					tmp = tempoSaidaCarros;
+				}
+				else {
+					status = 1;
+					y = 0;
+					tmp = tempoEntradaCarros;
+				}
+				inicioT = t;
+			}
 		},1000);
 		/*
 		for(var j = 0; j < 1; j++){
